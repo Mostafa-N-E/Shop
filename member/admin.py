6f25f9aa4ff07c9d51a80de4a8fbd06d47b3b29e
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-09-10 14:30:55 +0430

    rename application customer to member

---
+++ member/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Customer
+
+# Register models.
+
+# --------------------------------------------------CUSTOMER----------------------------------------------------------
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', ]
+    list_editable = ['address','postal_code','city',]
+    search_fields = ['phone_number', ]
