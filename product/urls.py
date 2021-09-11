35b29e05d9e00bdb5f33acc43de25baa17d8c5f6
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-09-11 21:02:40 +0430

    product view and url

--- product/urls.py
+++ product/urls.py
@@ -4,11 +4,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-
-    # student
-    path('student/class/', views.ClassShow.as_view(), name='STU_show_classes'),
-    path('student/class-detail/<int:pk>', views.ClassDetail.as_view(), name='STU_class_detail'),
-    path('student/select-classes/', views.SelectClass.as_view(), name='STU_select_classes'),
-    path('student/select-class/<int:pk>', views.SelectClassLesson, name='STU_select_class'),
-    path('student/rented-book/', views.RentedBook.as_view(), name='STU_show_rented_book'),
+    path('home/', views.Products.as_view(), name='show_products'),
+    path('product_detail/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -8,7 +8,7 @@ class Products(generic.ListView):
     def get_queryset(self):
         return Product.objects.all()
 
-class ClassDetail(generic.DetailView):
+class ProductDetail(generic.DetailView):
     model = Product
     template_name = 'product/product_detail_m.html'
     context_object_name = "classLesson"
