85c6940c25f12f6de5ee68ecfe4991d983d748ff
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-09-11 23:49:31 +0430

    product view and url

--- product/views.py
+++ product/views.py
@@ -1,14 +1,21 @@
+from django.shortcuts import render
 from django.views import generic
 
-from .models import Product
+from .models import Product, Category
 
 class Products(generic.ListView):
     model = Product
-    template_name = 'product/products.html'
-    def get_queryset(self):
-        return Product.objects.all()
+    template_name = 'base.html'
+
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        categories = Category.objects.all()
+        context = {'products': products, 'categories':categories,}
+        return render(request, 'base.html', context)
+
+
 
 class ProductDetail(generic.DetailView):
     model = Product
     template_name = 'product/product_detail_m.html'
-    context_object_name = "classLesson"
+    context_object_name = "product"
