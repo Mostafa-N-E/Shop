f69fca8f3c7e8ce50e763f974f33f3207ff5961d
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-09-11 20:46:16 +0430

    product view and url

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Product
+
+class Products(generic.ListView):
+    model = Product
+    template_name = 'product/products.html'
+    def get_queryset(self):
+        return Product.objects.all()
+
+class ClassDetail(generic.DetailView):
+    model = Product
+    template_name = 'product/product_detail_m.html'
+    context_object_name = "classLesson"
