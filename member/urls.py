efc6cd389c470ad7979db40a0a54e0d359ace703
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-09-21 10:25:44 +0430

    fix password_change

--- member/urls.py
+++ member/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('login/',views.LoginView.as_view(), name='customer_login' ),
     path('registration/',views.RegisterView.as_view(), name='customer_register' ),
-
+    path('password/change/',views.PasswordChangeView.as_view(), name='customer_password_change' ),
 ]
--- member/views.py
+++ member/views.py
@@ -6,5 +6,10 @@ from django.views.generic import TemplateView
 class LoginView(TemplateView):
     template_name = "member/login.html"
 
+
 class RegisterView(TemplateView):
     template_name = "member/register.html"
+
+
+class PasswordChangeView(TemplateView):
+    template_name = "member/password_change.html"
