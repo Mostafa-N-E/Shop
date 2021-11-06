648a690b107f13b2a222fdaca8c37c9caccca35b
Mostafa Nazari <84570342+Mostafa-N-E@users.noreply.github.com>
2021-11-06 13:00:33 +0330

    Update urls.py

--- djangoShop/urls.py
+++ djangoShop/urls.py
@@ -42,5 +42,5 @@ urlpatterns += i18n_patterns(
     path('basket/', include('basket.urls')),
     path('order/', include('order.urls')),
 )
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
